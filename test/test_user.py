895c970dcf62b5a17e74d0678a132e1d53056596
George D <georgedutra661@gmail.com>
2023-11-19 19:28:43 -0300

    create validation tests for user class

--- test/test_user.py
+++ test/test_user.py
@@ -1,1 +1,30 @@
-# Arquivo para testes unitários
+import unittest
+
+import sys 
+import os
+sys.path.append(os.path.abspath("./src")) # Adds src directory to python modules path.
+
+from user.user import User
+import exc.exceptions as exc
+
+class TestUser(unittest.TestCase):
+    def test_validate_cpf_empty(self):
+        """Testa se o CPF está vazio"""
+        self.assertRaises(exc.EmptyFieldError, User, "", "123456")     
+    
+    def test_validate_cpf_invalid_chars(self):
+        """Testa se o CPF contém caracteres inválidos"""
+        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-9A", "123456")     
+        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-9>", "123456")
+
+    def test_validate_cpf_invalid_lenght(self):
+        """Testa se o CPF contém o tamanho correto"""
+        self.assertRaises(exc.InvalidCPFError, User, "999", "123456")     
+        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-999", "123456")     
+    
+    def test_validate_password_empty(self):
+        """Testa se a senha está vazia"""
+        self.assertRaises(exc.EmptyFieldError, User, "999.999.999-99", "")
+    
+if __name__ == "__main__":
+    unittest.main()
