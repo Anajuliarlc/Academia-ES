0b5c1a4ddfe1c62dcf3781504c67143fc27c66f3
George D <georgedutra661@gmail.com>
2023-11-19 23:59:13 -0300

    Corrija casos de teste de user

--- test/test_user.py
+++ test/test_user.py
@@ -8,23 +8,26 @@ from user.user import User
 import exc.exceptions as exc
 
 class TestUser(unittest.TestCase):
+    def setUp(self):
+        self.user = User()
+
     def test_validate_cpf_empty(self):
         """Testa o caso do CPF estar vazio"""
-        self.assertRaises(exc.EmptyFieldError, User, "", "123456")     
+        self.assertRaises(exc.EmptyFieldError, self.user.login, "", "123456")     
     
     def test_validate_cpf_invalid_chars(self):
         """Testa o caso do CPF conter caracteres inválidos"""
-        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-9A", "123456")     
-        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-9>", "123456")
+        self.assertRaises(exc.InvalidCPFError, self.user.login, "999.999.999-9A", "123456")     
+        self.assertRaises(exc.InvalidCPFError, self.user.login, "999.999.999-9>", "123456")
 
     def test_validate_cpf_invalid_lenght(self):
         """Testa o caso do CPF ter tamanho incorreto"""
-        self.assertRaises(exc.InvalidCPFError, User, "999", "123456")     
-        self.assertRaises(exc.InvalidCPFError, User, "999.999.999-999", "123456")     
+        self.assertRaises(exc.InvalidCPFError, self.user.login, "999", "123456")     
+        self.assertRaises(exc.InvalidCPFError, self.user.login, "999.999.999-999", "123456")     
     
     def test_validate_password_empty(self):
         """Testa o caso da senha estar vazia"""
-        self.assertRaises(exc.EmptyFieldError, User, "999.999.999-99", "")
+        self.assertRaises(exc.EmptyFieldError, self.user.login, "999.999.999-99", "")
     
 if __name__ == "__main__":
     unittest.main()
