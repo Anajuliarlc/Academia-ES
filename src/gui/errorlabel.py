2bfd94b8b8c3631fdd83859f89271cf0ff06bd0a
Anajuliarlc <anajuliaritalc@gmail.com>
2023-11-22 04:27:01 -0300

    Change ErrorLabel specifications

--- src/gui/errorlabel.py
+++ src/gui/errorlabel.py
@@ -3,10 +3,10 @@ import sys
 sys.path.append("./src")
 
 class ErrorLabel(tk.Label):
-    def __init__(self, window, text, pos_x, pos_y, width = 500, height = 80, font = ("Arial", 28)):
+    def __init__(self, window, text, pos_x, pos_y, width = 400, height = 80, font = ("Arial", 14)):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.width = width
         self.height = height
-        super().__init__(window, text = text, font = font, bg = "#000F31", fg = "#FEFAD2")
+        super().__init__(window, text = text, font = font, bg = "#DF8350", fg = "#000F31")
         self.place(x = self.pos_x, y = self.pos_y, width = self.width, height = self.height)
