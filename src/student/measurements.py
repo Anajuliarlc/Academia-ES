ca901607e2b1e51ecb5d25886ce50bddb6bd0939
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2023-11-22 03:41:48 -0300

    create class measurements

---
+++ src/student/measurements.py
@@ -0,0 +1,7 @@
+
+class Measurements():
+    def __init__(self, user) -> None:
+        self.user = user
+
+    def update_db(self, measurements: dict) -> bool:
+        pass
--- test/test_measurements_frame.py
+++ test/test_measurements_frame.py
@@ -1,23 +1,22 @@
 import unittest
 import sys
 sys.path.append("./src")
-import student.measurements_frame as mf
+import student.measurements as mf
 import tkinter as tk
 
-class TestMeasurementsFrame(unittest.TestCase):
+class TestMeasurements(unittest.TestCase):
     window = tk.Tk()
 
     def test_update_db_happy_case(self):
         """ Verify if the update_db method is working in the happy case """        
-        measurements = {"IdUser": 3,
-                        "Weight": 80,
+        measurements = {"Weight": 80,
                         "Height": 1.80,	
                         "HighWaist": 65,
                         "LowWaist": 65,
                         "Bust": 90,
                         "Biceps": 30,
                         "Thigh": 50}
-        frame = mf.MeasurementsFrame(tk.Tk())
+        frame = mf.Measurements(3)
         self.assertTrue(frame.update_db(measurements))
 
 
