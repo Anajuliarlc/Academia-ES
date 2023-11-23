fc32d0233a65e86a0c52273a961f070730bb63e6
Luan Rodrigues de Carvalho <105639491+Luan-vht3@users.noreply.github.com>
2023-11-22 21:26:26 -0300

    Add the connection with the database

--- src/student/set_goals_frame.py
+++ src/student/set_goals_frame.py
@@ -1,116 +1,258 @@
 import tkinter as tk
+import time
+
 import sys
-sys.path.append("./src")
+import os
+# Adds src directory to python modules path.
+sys.path.append(os.path.abspath("./src")) 
+
 import gui.frame as fr
+import pandas as pd
+from main import System
+from gui.buttons import DefaultButton
+from gui.errorlabel import ErrorLabel
 
 class SetGoalsFrame(fr.Frame):
-    def __init__(self, window: tk.Tk, height: int = 600, width: int = 800,
-                 pos_x: int = 0, pos_y: int = 0) -> None:
+    def __init__(self, window: tk.Tk, height: int = 600, width: int = 2*480,
+                 pos_x: int = 720, pos_y: int = 200) -> None:
+        self.system = System()
+
         super().__init__(window, height, width, pos_x, pos_y)
 
     def design(self) -> None:
-        pass
-
-    def clear_error_messages(self) -> None:
-        try:
-            if len(self.error_messages) > 0:
-                for message in self.error_messages:
-                    message.destroy()
-        except AttributeError:
-            pass
+        self.config(bg = "#000F31")
     
     def place_objects(self) -> None:
-        self.label = tk.Label(self, text="Definir metas de treino", font=("Arial", 20))
-        self.label_goal_name = tk.Label(self, text = "Nome da meta")
-        self.entry_goal_name = tk.Entry(self)
+        """Place objects on the frame
+        """
 
-        self.clicked_time = tk.StringVar()
-        self.clicked_time.set("Prazo da meta")
+        self.label1 = tk.Label(self,
+                               text = "Especifique sua meta",
+                               font = ("Arial", 20, "bold"),
+                               bg = "#000F31",
+                               fg = "#FEFAD2")
+        self.label1.place(x = 0, y = 20, height = 30, width = 480)
 
-        self.label_goal_time = tk.Label(self, text = "Prazo da meta")
-        self.entry_goal_time = tk.Entry(self)
-        self.drop_goal_time = tk.OptionMenu(
-            self, self.clicked_time,
-            "Dias",
-            "Semanas",
-            "Meses",
-            "Semestres",
-            "Anos",
-        )
+        #############################################################
 
-        self.label_goal_description = tk.Label(self, text = "Descrição da\nmeta")
-        self.entry_goal_description = tk.Entry(self)
+        self.label_goal_name = tk.Label(self,
+                                        text = "Nome da meta",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_goal_name.place(x = 50, y = 70, height = 20)
+        self.entry_goal_name = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_goal_name.place(x = 50, y = 90, height = 30, width = 380)
 
-        self.button = tk.Button(self, text = "Definir meta")
+        #############################################################
+
+        self.label_cardio_min = tk.Label(self,
+                                        text = "Mínimo de aeróbicos (opcional)",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_cardio_min.place(x = 50, y = 130, height = 20)
+        self.entry_cardio_min = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_cardio_min.place(x = 50, y = 150, height = 30, width = 180)
+
+        #############################################################
+
+        self.label_goal_weight = tk.Label(self,
+                                        text = "Objetivo de peso (opcional)",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_goal_weight.place(x = 250, y = 130, height = 20)
+        self.entry_goal_weight = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_goal_weight.place(x = 250, y = 150, height = 30, width = 180)
+
+        #############################################################
         
-        self.clicked_type = tk.StringVar()
-        self.clicked_type.set("Tipo de meta")
-
-        self.label_goal_type = tk.Label(self, text = "Tipo de meta")
-        self.drop_goal_type = tk.OptionMenu(
-            self, self.clicked_type,
-            "Perda de peso",
-            "Ganho de massa muscular",
-            "Melhora de condicionamento físico",
-            "Melhora de flexibilidade e mobilidade",
-            "Melhora de resistência cardiovascular",
-            "Desempenho esportivo",
-            "Saúde e bem-estar em geral",
-            "Outro",
-        )
+        self.label_start_date = tk.Label(self,
+                                        text = "Data de início",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_start_date.place(x = 50, y = 190, height = 20)
+        self.entry_start_date = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_start_date.place(x = 50, y = 210, height = 30, width = 180)
         
-        def set_goals() -> None:
-            self.clear_error_messages()
-            self.error_messages = []
+        #############################################################
+
+        self.label_final_date = tk.Label(self,
+                                        text = "Data final",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_final_date.place(x = 250, y = 190, height = 20)
+        self.entry_final_date = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_final_date.place(x = 250, y = 210, height = 30, width = 180)
+
+        #############################################################
+        
+        self.label_lean_mass = tk.Label(self,
+                                        text = "Massa magra (opcional)",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_lean_mass.place(x = 50, y = 250, height = 20)
+        self.entry_lean_mass = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_lean_mass.place(x = 50, y = 270, height = 30, width = 180)
+
+        #############################################################
+
+        self.label_fat_mass = tk.Label(self,
+                                        text = "Massa gorda (opcional)",
+                                        font = ("Arial", 10),
+                                        bg = "#000F31",
+                                        fg = "#FEFAD2")
+        self.label_fat_mass.place(x = 250, y = 250, height = 20)
+        self.entry_fat_mass = tk.Entry(self,
+                                        font = ("Arial", 10),
+                                        bg = "#DF8350",
+                                        fg = "#FEFAD2",)
+        self.entry_fat_mass.place(x = 250, y = 270, height = 30, width = 180)
+
+        #############################################################
+
+        self.warning = tk.Label()
+
+        def confirm_set_goals() -> None:
+            self.warning.destroy()
 
             if self.entry_goal_name.get() == "":
-                self.error_messages.append(tk.Label(self, text = "Dê um nome para a meta.",
-                                              font = ("Arial", 10), fg = "red"))
-            if self.clicked_type.get() == "Tipo de meta":
-                self.error_messages.append(tk.Label(self, text = "Selecione o tipo da meta.",
-                                              font = ("Arial", 10), fg = "red"))
-            if self.clicked_time.get() == "Prazo da meta":
-                self.error_messages.append(tk.Label(self, text = "Selecione o prazo da meta.",
-                                              font = ("Arial", 10), fg = "red"))
-            if not self.entry_goal_time.get().isnumeric():
-                self.error_messages.append(tk.Label(self, text = "Defina um prazo para a meta.",
-                                              font = ("Arial", 10), fg = "red"))
-            if self.entry_goal_description.get() == "":
-                self.error_messages.append(tk.Label(self, text = "Descreva a meta.",
-                                              font = ("Arial", 10), fg = "red"))
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira um nome para a meta",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif (self.entry_cardio_min.get() != ""
+                  and not self.entry_cardio_min.get().isnumeric()):
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira um valor numérico para o mínimo de aeróbicos",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif (self.entry_goal_weight.get() != ""
+                  and not self.entry_goal_weight.get().isnumeric()):
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira um valor numérico para o objetivo de peso",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif self.entry_start_date.get() == "":
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira uma data de início",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif self.entry_final_date.get() == "":
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira uma data final",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif (self.entry_lean_mass.get() != ""
+                    and not self.entry_lean_mass.get().isnumeric()):
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira um valor numérico \
+                                            para a massa magra",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
+                
+            elif (self.entry_fat_mass.get() != ""
+                  and not self.entry_fat_mass.get().isnumeric()):
+                self.warning = ErrorLabel(window = self,
+                                          text = "Insira um valor numérico \
+                                            para a massa gorda",
+                                          pos_x = 50, pos_y = 360,
+                                          width = 380, height = 30,
+                                          font = ("Arial", 10))
             
-            y_pos = 350
-            for message in self.error_messages:
-                message.place(x = 200, y = y_pos, height = 40, width = 400)
-                y_pos += 40
+            else:
+                # Falta a função para inserir os dados no bd
+                database = self.system.database
+                goals = pd.DataFrame({"IdUser": [self.system.user],
+                                      "CardioMin": [self.entry_cardio_min.get()],
+                                      "GoalWeight": [self.entry_goal_weight.get()],
+                                      "GoalDate": [self.entry_start_date.get()],
+                                      "FinalDate": [self.entry_final_date.get()],
+                                      "LeanMassPct": [self.entry_lean_mass.get()],
+                                      "FatPct": [self.entry_fat_mass.get()]})
+                self.update_db(goals)
 
-            if len(self.error_messages) == 0:
-                # falta a função para mandar para o bd
+                self.label1.destroy()
+                self.label_goal_name.destroy()
+                self.entry_goal_name.destroy()
+                self.label_cardio_min.destroy()
+                self.entry_cardio_min.destroy()
+                self.label_goal_weight.destroy()
+                self.entry_goal_weight.destroy()
+                self.label_start_date.destroy()
+                self.entry_start_date.destroy()
+                self.label_final_date.destroy()
+                self.entry_final_date.destroy()
+                self.label_lean_mass.destroy()
+                self.entry_lean_mass.destroy()
+                self.label_fat_mass.destroy()
+                self.entry_fat_mass.destroy()
+                self.button_confirm.destroy()
                 self.destroy()
-                import student_frame_factory as sff
-                sff.StudentFrameFactory("ThankYouCard", self.window)
 
-        self.button["command"] = set_goals
+                #import student_frame_factory as sff
+                #sff.StudentFrameFactory("ThankYouCard", self.window)
 
-        self.label.place(x = 200, y = 20, height = 40, width = 400)
+        self.button_confirm = DefaultButton(text = "Confirmar",
+                                            command = confirm_set_goals,
+                                            window = self,
+                                            pos_x = 50, pos_y = 320,
+                                            height = 30, width = 380,
+                                            font = ("Arial", 10),)
 
-        self.label_goal_name.place(x = 200, y = 100, height = 40, width = 100)
-        self.entry_goal_name.place(x = 300, y = 100, height = 40, width = 300)
-
-        self.drop_goal_type.place(x = 300, y = 140, height = 40, width = 300)
-        self.label_goal_type.place(x = 200, y = 140, height = 40, width = 100)
-
-        self.label_goal_time.place(x = 200, y = 180, height = 40, width = 100)
-        self.entry_goal_time.place(x = 300, y = 180, height = 40, width = 175)
-        self.drop_goal_time.place(x = 475, y = 180, height = 40, width = 125)
-        
-        self.label_goal_description.place(x = 200, y = 220, height = 80, width = 100)
-        self.entry_goal_description.place(x = 300, y = 220, height = 80, width = 300)
-        
-        self.button.place(x = 200, y = 300, height = 40, width = 400)
-
+    
     def destroy(self) -> None:
         super().destroy()
     
     def update_db(self, goals: dict) -> bool:
-        pass
+        """Updates the database with the new goals
+        """
+        table = "Goal (IdUser, CardioMin, GoalWeight, GoalDate, FinalDate, \
+                LeanMassPct, FatPct)"
+        
+        date = time.strftime("%Y-%m-%d")
+        values = f"({self.system.user}, '{goals['CardioMin']}', \
+                   '{goals['GoalWeight']}', '{goals['GoalDate']}', \
+                   '{goals['FinalDate']}', '{goals['LeanMassPct']}', \
+                   '{goals['FatPct']}')"
+        
+        result = self.system.database.insert(table = table, values = values)
+        if "1 row affected" in result:
+            print("Meta inserida com sucesso")
+            return True
+        else:
+            print("Erro ao inserir meta")
+            return False
