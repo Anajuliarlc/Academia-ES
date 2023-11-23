2e0bd08b8efb1d78b7f388782f60a08ffb592110
LuisHDBueno <105614892+LuisHDBueno@users.noreply.github.com>
2023-11-22 23:07:09 -0300

    Fix the test cases

--- test/test_classes.py
+++ test/test_classes.py
@@ -11,23 +11,31 @@ class TestClasses(unittest.TestCase):
     classes = cl.Classes()
     classes.system.user = 1
     def test_case_insert_happy_case(self):
+        """ If the user insert a class in the database, the system should
+        return True
+        """
         name = "Spining"
         date = "2020-12-12 12:00:00"
         description = "Aula de spining"
         max_students = 10
         self.assertTrue(self.classes.insert_class(name, date,
                                                    description, max_students))
-        #classes.system.database.delete("Class", "ClassName", name)
+        condition = "WHERE ClassName = '" + name + "'"
+        self.classes.system.database.delete("Class", condition)
 
     def test_case_insert_time_conflit(self):
+        """ If the user try to insert a class in the same time of another class
+        the system should raise a TimeConflictError
+        """        
         name = "Spining"
         date = "2020-12-12 12:00:00"
         description = "Aula de spining"
         self.classes.insert_class(name, date, description)
         self.assertRaises(ex.TimeConflictError, self.classes.insert_class,
                            name, date, description)
-        #self.classes.system.database.delete("Class", "ClassName", name)
-        #self.classes.system.database.delete("Class", "ClassName", name)
+        condition = "WHERE ClassName = '" + name + "'"
+        self.classes.system.database.delete("Class", condition)
+        self.classes.system.database.delete("Class", condition)
 
 if __name__ == "__main__":
     unittest.main()
