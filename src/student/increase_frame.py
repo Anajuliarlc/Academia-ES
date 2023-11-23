586e48a44c732f3438b3177ea90735a228287a32
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-11-23 02:25:40 -0300

    Delete all frames when other is opened

--- src/student/increase_frame.py
+++ src/student/increase_frame.py
@@ -9,7 +9,6 @@ import db.db_connector as db
 import gui.errorlabel as el
 import student.thank_you_frame as tyf
 import main
-import student.increase as inc
 
 class IncreaseFrame(fr.Frame):
     def __init__(self, window: tk.Tk, height: int = 400, width: int = 960, 
@@ -18,6 +17,23 @@ class IncreaseFrame(fr.Frame):
 
     def design(self) -> None:
         self.config(bg = "#000F31")
+
+    def button_send_request(self):
+        if self.clicked.get() == "Exercício a progredir":
+            # show error message
+            el.ErrorLabel(self, "Selecione um exercício", pos_x=280,
+                                    pos_y=300, width=400, height=50)
+        else:
+            system = main.System()
+            return_message = system.database.query(f"UPDATE Exercise SET WeightExercise = WeightExercise + 2.5 WHERE ExerciseName = \'{self.clicked.get()}\' AND IdUser = {system.user};")
+            print(return_message)
+                
+            for frame in self.window.active_frames:
+                if type(frame).__name__ != "MenuFrame" and type(frame).__name__ != "LogoFrame":
+                    frame.destroy()
+                    
+            sff.StudentFrameFactory.get_frame("ThankYouFrame", self.window)
+
 
     def place_objects(self) -> None:
         # select the student's workouts
@@ -47,7 +63,7 @@ class IncreaseFrame(fr.Frame):
         self.button_request_change = bt.MenuButton(text = "Progredir",
                                         window = self, pos_x = 280,
                                         pos_y = 240, width=400, height=50,
-                                        command = lambda: inc.send_request(self))
+                                        command = self.button_send_request)
 
     def destroy(self) -> None:
         super().destroy()
--- src/student/workouts.py
+++
@@ -1,9 +0,0 @@
-import student.student_frame_factory as sff
-
-def request_change(frame) -> None:
-        sff.StudentFrameFactory.get_frame("RequestChangeFrame", frame.window)
-        frame.destroy()
-
-def go_to_increase(frame) -> None:
-            sff.StudentFrameFactory.get_frame("IncreaseFrame", frame.window)
-            frame.destroy()
