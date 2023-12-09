fae96a55ac80fb1c4ba0ffa43d5f4c5345a8e51c
Iago Riveiro Santos Dutra <iagorivsd@gmail.com>
2023-12-09 15:15:40 -0300

    Doctest System

--- src/main.py
+++ src/main.py
@@ -1,17 +1,39 @@
 from db.db_connector import DBConnector
+from typing import Any, Dict, Type, TypeVar
+
+T = TypeVar('T', bound='SystemMeta')
 
 class SystemMeta(type):
-    """ System Metaclass for Singleton Pattern application. """
-
-    _instances = {}
+    """ System Metaclass for Singleton Pattern application. 
     
-    def __call__(cls, *args, **kwargs):
+    >>> class Test(metaclass=SystemMeta): pass
+    >>> a = Test()
+    >>> b = Test()
+    >>> a is b
+    True
+    """
+
+    _instances: Dict[Type[T], T] = {}
+    
+    def __call__(cls: Type[T], *args: Any, **kwargs: Any) -> T:
         if cls not in cls._instances:
             instance = super().__call__(*args, **kwargs)
             cls._instances[cls] = instance
         return cls._instances[cls]
 
 class System(metaclass = SystemMeta):
+    """ 
+    System class for Singleton Pattern application. 
+
+    >>> system1 = System()
+    >>> system2 = System()
+    >>> system1 is system2
+    True
+
+    >>> system1.database
+    <db.db_connector.DBConnector object at ...>
+    """
+    
     def __init__(self) -> None:
         self.user = None
         self.window = None
@@ -22,5 +44,5 @@ class System(metaclass = SystemMeta):
         ... #TODO: Implement the window start
         
 if __name__ == "__main__":
-    system = System()
-    system.start()
+    import doctest
+    doctest.testmod(optionflags=doctest.ELLIPSIS)
